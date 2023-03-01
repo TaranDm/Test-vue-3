e63b1d28243721369baab7279d54d14ea4d52a01
dmitriy <dmitriy.taran1@gmail.com>
2023-03-01 17:59:38 +0200

    add actions

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import { createPinia } from "pinia";
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(router, pinia);
+app.use(router);
+app.use(pinia);
 app.mount("#app");
--- src/stores/Store.js
+++ src/stores/Store.js
@@ -4,12 +4,43 @@ export const useStore = defineStore("Store", {
     movies: [
       {
         id: 1,
-        name: "Spider-Man",
+        original_title: "Spider-Man",
+        overview:
+          "After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.",
+        poster_path: "/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
+        release_date: "2002-05-01",
+        isWatched: false,
       },
       {
         id: 2,
-        name: "Spider-Man",
+        original_title: "The Batman",
+        overview:
+          "In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.",
+        poster_path: "/b0PlSFdDwbyK0cf5RxwDpaOJQvQ.jpg",
+        release_date: "2022-03-01",
+        isWatched: true,
       },
     ],
+    activeTab: 1,
   }),
+  actions: {
+    setActiveTab(id) {
+      this.activeTab = id;
+    },
+    toggleWatched(id) {
+      const idx = this.movies.findIndex((el) => el.id === id);
+      this.movies[idx].isWatched = !this.movies[idx].isWatched;
+    },
+    deleteMovie(id) {
+      this.movies = this.movies.filter((el) => el.id !== id);
+    },
+  },
+  getters: {
+    watchMovies() {
+      return this.movies.filter((el) => el.isWatched);
+    },
+    totalCountMovies() {
+      return this.movies.length;
+    },
+  },
 });
